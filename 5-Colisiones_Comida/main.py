aad1616596acc4ec3cd6b6b9f7e275c42e6aec6f
Rafael Fuentes <rafafuentesdev@gmail.com>
2023-03-29 14:03:12 +0200

    colisiones_comida

---
+++ 5-Colisiones_Comida/main.py
@@ -0,0 +1,34 @@
+#main.py
+import turtle
+import time
+
+from serpiente import Serpiente
+from comida import Comida
+
+ventana = turtle.Screen()
+ventana.setup(width=600, height=600)
+ventana.bgcolor("black")
+ventana.title("Snake Game")
+
+ventana.tracer(0)
+
+serpiente = Serpiente() ##---------- CREAR LA SERPIENTE ----------##
+comida = Comida()       ##---------- CREAR LA COMIDA -------------##
+
+ventana.listen()
+ventana.onkey(serpiente.arriba, "Up")
+ventana.onkey(serpiente.abajo, "Down")
+ventana.onkey(serpiente.izquierda, "Left")
+ventana.onkey(serpiente.derecha, "Right")
+
+en_juego = True
+while en_juego:
+    ventana.update()
+    time.sleep(0.1)
+    serpiente.mover() ##---------- MOVER LA SERPIENTE ----------##
+    
+    ##----------DETECTAR COLISIONES COMIDA ----------##
+    if serpiente.cabeza.distance(comida) < 15:
+        comida.actualizar()
+    
+ventana.exitonclick()
